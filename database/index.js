5ba597b2d8e947e6f10de7d7cfd8ce3dfa399ce2
ananddhakane01 <ananddhakane01@gmail.com>
2022-02-05 18:48:59 +0530

    ok

--- database/index.js
+++ database/index.js
@@ -4,21 +4,11 @@ require("dotenv").config();
 let sequelize;
 
 if (process.env.ENVIRONMENT == "development") {
-  console.log();
-  console.log();
-  console.log("in development");
-  console.log();
-  console.log();
   sequelize = new Sequelize("stonksdb", "postgres", "asdf12345", {
     host: "localhost",
     dialect: "postgres" /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
   });
 } else {
-  console.log();
-  console.log();
-  console.log("in production");
-  console.log();
-  console.log();
   sequelize = new Sequelize(process.env.POSTGRES_URI);
 }
 
