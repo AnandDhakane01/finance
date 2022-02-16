11bb7f2d47f53e95330de447727ea595b70aad89
ananddhakane01 <ananddhakane01@gmail.com>
2022-02-16 11:42:32 +0530

    cors

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const indexRouter = require("./routes/index");
 
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.use(cookieParser());
 app.use(express.json());
